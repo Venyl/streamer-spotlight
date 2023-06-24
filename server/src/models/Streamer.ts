3cdc4937a5a7b0edf9d9cbe1645c8bf21af0ebcf
Venyl <zexe2121@gmail.com>
2023-06-24 14:42:23 +0200

    create the mongoose streamer model

---
+++ server/src/models/Streamer.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+export interface IStreamer {
+    name: string;
+    platform: 'Twitch' | 'YouTube' | 'TikTok' | 'Kick' | 'Rumble';
+    description: string;
+    upvotes: number;
+    downvotes: number;
+}
+
+const StreamerSchema = new Schema<IStreamer>({
+    name: ObjectId,
+    platform: {
+        type: String,
+        enum: ['Twitch', 'YouTube', 'TikTok', 'Kick', 'Rumble'],
+    },
+    description: String,
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+});
+
+const Streamer = mongoose.model<IStreamer>('Streamer', StreamerSchema);
+
+export default Streamer;
