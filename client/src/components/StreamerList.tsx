8672baeac4fb2e5dd9e6cda1af0ac7e01d478b15
Venyl <zexe2121@gmail.com>
2023-06-24 17:30:59 +0200

    refactor the StreamerList component

--- client/src/components/StreamerList.tsx
+++ client/src/components/StreamerList.tsx
@@ -1,23 +1,32 @@
-import { IStreamerWithVotes } from '../lib/types';
+import { useQuery } from '@tanstack/react-query';
+import getAllStreamers from '../api/getAllStreamers';
 
-type Props = {
-    streamers: IStreamerWithVotes[];
-};
+export default function StreamerList() {
+    const {
+        data: streamers,
+        isLoading,
+        isError,
+    } = useQuery(['text'], getAllStreamers);
 
-export default function StreamerList({ streamers }: Props) {
     return (
-        <ul>
-            {streamers.map(streamer => (
-                <li key={streamer.name}>
-                    <h2>{streamer.name}</h2>
-                    <p>{streamer.platform}</p>
-                    <p>{streamer.description}</p>
-                    <div>
-                        <span>Up: {streamer.upvotes}</span>
-                        <span>Down: {streamer.downvotes}</span>
-                    </div>
-                </li>
-            ))}
-        </ul>
+        <aside>
+            {isLoading && <p>Loading...</p>}
+            {isError && <p>Error!</p>}
+            {streamers && (
+                <ul>
+                    {streamers.map(streamer => (
+                        <li key={streamer.name}>
+                            <h2>{streamer.name}</h2>
+                            <p>{streamer.platform}</p>
+                            <p>{streamer.description}</p>
+                            <div>
+                                <span>Up: {streamer.upvotes}</span>
+                                <span>Down: {streamer.downvotes}</span>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </aside>
     );
 }
