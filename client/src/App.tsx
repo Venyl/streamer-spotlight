b42850f5c8565fa28455b769b1f04f9b3da1474e
Venyl <zexe2121@gmail.com>
2023-06-24 17:34:11 +0200

    extract the streamer form into it's own component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,79 +1,17 @@
 import StreamerList from './components/StreamerList';
-import addStreamer from './api/addStreamer';
-import { IStreamer } from './lib/types';
 import { useState } from 'react';
 import IssueList from './components/IssueList';
+import StreamerForm from './components/StreamerForm';
 
 function App() {
-    const [streamer, setStreamer] = useState<IStreamer>({
-        name: '',
-        platform: 'Twitch',
-        description: '',
-    });
-
     const [issues, setIssues] = useState<string[]>([]);
-
-    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        const resData = await addStreamer(streamer);
-        setIssues(resData.issues);
-        setStreamer({ name: '', platform: 'Twitch', description: '' });
-    }
 
     return (
         <main>
             <StreamerList />
 
             <IssueList issues={issues} />
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name</label>
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    required
-                    onChange={e =>
-                        setStreamer(streamer => ({
-                            ...streamer,
-                            name: e.target.value,
-                        }))
-                    }
-                />
-
-                <label htmlFor="platform">Platform</label>
-                <select
-                    name="platform"
-                    id="platform"
-                    required
-                    onChange={e =>
-                        setStreamer(streamer => ({
-                            ...streamer,
-                            platform: e.target.value as
-                                | 'Twitch'
-                                | 'YouTube'
-                                | 'TikTok'
-                                | 'Kick'
-                                | 'Rumble',
-                        }))
-                    }
-                >
-                    <option>Twitch</option>
-                    <option>YouTube</option>
-                    <option>TikTok</option>
-                    <option>Kick</option>
-                    <option>Rumble</option>
-                </select>
-
-                <label htmlFor="description">Name</label>
-                <input
-                    type="text"
-                    name="description"
-                    id="description"
-                    required
-                />
-
-                <button>Add</button>
-            </form>
+            <StreamerForm setIssues={setIssues} />
         </main>
     );
 }
---
+++ client/src/components/StreamerForm.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import addStreamer from '../api/addStreamer';
+import { IStreamer } from '../lib/types';
+
+type Props = {
+    setIssues: React.Dispatch<React.SetStateAction<string[]>>;
+};
+
+export default function StreamerForm({ setIssues }: Props) {
+    const [streamer, setStreamer] = useState<IStreamer>({
+        name: '',
+        platform: 'Twitch',
+        description: '',
+    });
+
+    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const resData = await addStreamer(streamer);
+        setIssues(resData.issues);
+        setStreamer({ name: '', platform: 'Twitch', description: '' });
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input
+                type="text"
+                name="name"
+                id="name"
+                required
+                onChange={e =>
+                    setStreamer(streamer => ({
+                        ...streamer,
+                        name: e.target.value,
+                    }))
+                }
+            />
+
+            <label htmlFor="platform">Platform</label>
+            <select
+                name="platform"
+                id="platform"
+                required
+                onChange={e =>
+                    setStreamer(streamer => ({
+                        ...streamer,
+                        platform: e.target.value as
+                            | 'Twitch'
+                            | 'YouTube'
+                            | 'TikTok'
+                            | 'Kick'
+                            | 'Rumble',
+                    }))
+                }
+            >
+                <option>Twitch</option>
+                <option>YouTube</option>
+                <option>TikTok</option>
+                <option>Kick</option>
+                <option>Rumble</option>
+            </select>
+
+            <label htmlFor="description">Name</label>
+            <input type="text" name="description" id="description" required />
+
+            <button>Add</button>
+        </form>
+    );
+}
