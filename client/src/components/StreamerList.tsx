8eb13c890c37d038ba2862c3ad9e6c0de4378b80
Venyl <zexe2121@gmail.com>
2023-06-24 17:23:35 +0200

    display more streamer details

--- client/src/components/StreamerList.tsx
+++ client/src/components/StreamerList.tsx
@@ -8,7 +8,15 @@ export default function StreamerList({ streamers }: Props) {
     return (
         <ul>
             {streamers.map(streamer => (
-                <li key={streamer.name}>{streamer.name}</li>
+                <li key={streamer.name}>
+                    <h2>{streamer.name}</h2>
+                    <p>{streamer.platform}</p>
+                    <p>{streamer.description}</p>
+                    <div>
+                        <span>Up: {streamer.upvotes}</span>
+                        <span>Down: {streamer.downvotes}</span>
+                    </div>
+                </li>
             ))}
         </ul>
     );
