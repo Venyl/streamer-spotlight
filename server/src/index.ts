27f305fc701c3a4389dbe0c3a0ebd65607d7084e
Venyl <zexe2121@gmail.com>
2023-06-24 14:55:13 +0200

    implement the GET and POST /streamers endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import socketio from 'socket.io';
+import Streamer, { IStreamer } from './models/Streamer';
 
 const app = express();
 if (!process.env.MONGO_URI) {
@@ -12,8 +13,23 @@ const db = mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get('/streamers', async (req: Request, res: Response) => {
+    res.json(await Streamer.find());
 });
+
+app.post(
+    '/streamers',
+    async (req: Request<{}, {}, IStreamer>, res: Response) => {
+        const { name, platform, description } = req.body;
+        const newStreamer = new Streamer<IStreamer>({
+            name,
+            platform,
+            description,
+            upvotes: 0,
+            downvotes: 0,
+        });
+        await newStreamer.save();
+    }
+);
 
 app.listen(5000);
