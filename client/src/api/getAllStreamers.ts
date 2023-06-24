e8d97a8b0df056f83d0d445f3ec305371c5005b5
Venyl <zexe2121@gmail.com>
2023-06-24 15:35:51 +0200

    replace https with http in the API function

--- client/src/api/getAllStreamers.ts
+++ client/src/api/getAllStreamers.ts
@@ -1,7 +1,7 @@
 import { IStreamer } from '../lib/types';
 
 export default async function getAllStreamers() {
-    const data = await fetch('https://localhost:5000/streamers');
+    const data = await fetch('http://localhost:5000/streamers');
     const streamers: IStreamer[] = await data.json();
     return streamers;
 }
