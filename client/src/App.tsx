338dbcd8dad7d66a0849bb484f645a83da19f33a
Venyl <zexe2121@gmail.com>
2023-06-24 15:14:17 +0200

    create a basic form for adding streamers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,45 @@
 import { useQuery } from '@tanstack/react-query';
+import getAllStreamers from './api/getAllStreamers';
+import StreamerList from './components/StreamerList';
 
 function App() {
-    const { data, isLoading, isError, error } = useQuery(['text'], () =>
-        fetch('http://localhost:5000/').then(res => res.text())
-    );
+    const {
+        data: streamers,
+        isLoading,
+        isError,
+        error,
+    } = useQuery(['text'], getAllStreamers);
 
     return (
         <main>
-            {isLoading && <p>Loading...</p>}
-            {isError && <p>Error! {error as string}</p>}
-            {data && <p>{data}</p>}
+            <aside>
+                {isLoading && <p>Loading...</p>}
+                {isError && <p>Error! {error as string}</p>}
+                {streamers && (
+                    <p>
+                        <StreamerList streamers={streamers} />
+                    </p>
+                )}
+            </aside>
+
+            <form>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" />
+
+                <label htmlFor="platform">Platform</label>
+                <select name="platform" id="platform">
+                    <option>Twitch</option>
+                    <option>YouTube</option>
+                    <option>TikTok</option>
+                    <option>Kick</option>
+                    <option>Rumble</option>
+                </select>
+
+                <label htmlFor="description">Name</label>
+                <input type="text" name="description" id="description" />
+
+                <button>Add</button>
+            </form>
         </main>
     );
 }
