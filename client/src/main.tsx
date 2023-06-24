504ca126d98b08d254ed5f13b3b65546c191463f
Venyl <zexe2121@gmail.com>
2023-06-24 18:17:51 +0200

    overhaul routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
+import Layout from './Layout.tsx';
+import StreamerPage from './components/StreamerPage.tsx';
 
 const queryClient = new QueryClient();
 
@@ -11,7 +13,15 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
-                <App />
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<App />} />
+                        <Route
+                            path="streamer/:streamerName"
+                            element={<StreamerPage />}
+                        />
+                    </Route>
+                </Routes>
             </BrowserRouter>
         </QueryClientProvider>
     </React.StrictMode>
