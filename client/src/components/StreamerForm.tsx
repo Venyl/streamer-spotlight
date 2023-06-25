9bb8e628e56886f6d3a11713fb070d2c7e3ce249
Venyl <zexe2121@gmail.com>
2023-06-25 20:30:01 +0200

    add form styling

--- client/src/components/StreamerForm.tsx
+++ client/src/components/StreamerForm.tsx
@@ -24,74 +24,71 @@ export default function StreamerForm() {
 
     return (
         <form onSubmit={handleSubmit}>
-            <IssueList issues={issues} />
-
-            <h1 className="form-title">Add a streamer</h1>
-
-            <div className="input-container">
-                <label htmlFor="name">Name</label>
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Asmongold"
-                    required
-                    value={streamer.name}
-                    onChange={e =>
-                        setStreamer(streamer => ({
-                            ...streamer,
-                            name: e.target.value,
-                        }))
-                    }
-                />
+            <div className="wrapper">
+                <IssueList issues={issues} />
+                <h1 className="form-title">Add a streamer</h1>
+                <div className="input-container">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Asmongold"
+                        required
+                        value={streamer.name}
+                        onChange={e =>
+                            setStreamer(streamer => ({
+                                ...streamer,
+                                name: e.target.value,
+                            }))
+                        }
+                    />
+                </div>
+                <div className="input-container">
+                    <label htmlFor="platform">Platform</label>
+                    <select
+                        name="platform"
+                        id="platform"
+                        required
+                        value={streamer.platform}
+                        onChange={e =>
+                            setStreamer(streamer => ({
+                                ...streamer,
+                                platform: e.target.value as
+                                    | 'Twitch'
+                                    | 'YouTube'
+                                    | 'TikTok'
+                                    | 'Kick'
+                                    | 'Rumble',
+                            }))
+                        }
+                    >
+                        <option selected>Twitch</option>
+                        <option>YouTube</option>
+                        <option>TikTok</option>
+                        <option>Kick</option>
+                        <option>Rumble</option>
+                    </select>
+                </div>
+                <div className="input-container">
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                        name="description"
+                        id="description"
+                        placeholder="Asmongold is an American Twitch streamer..."
+                        rows={5}
+                        required
+                        value={streamer.description}
+                        onChange={e =>
+                            setStreamer(streamer => ({
+                                ...streamer,
+                                description: e.target.value,
+                            }))
+                        }
+                    />
+                </div>
+                <button type="submit">+ Add</button>
             </div>
-
-            <div className="input-container">
-                <label htmlFor="platform">Platform</label>
-                <select
-                    name="platform"
-                    id="platform"
-                    required
-                    value={streamer.platform}
-                    onChange={e =>
-                        setStreamer(streamer => ({
-                            ...streamer,
-                            platform: e.target.value as
-                                | 'Twitch'
-                                | 'YouTube'
-                                | 'TikTok'
-                                | 'Kick'
-                                | 'Rumble',
-                        }))
-                    }
-                >
-                    <option selected>Twitch</option>
-                    <option>YouTube</option>
-                    <option>TikTok</option>
-                    <option>Kick</option>
-                    <option>Rumble</option>
-                </select>
-            </div>
-
-            <div className="input-container">
-                <label htmlFor="description">Description</label>
-                <textarea
-                    name="description"
-                    id="description"
-                    placeholder="Asmongold is an American Twitch streamer..."
-                    rows={5}
-                    required
-                    value={streamer.description}
-                    onChange={e =>
-                        setStreamer(streamer => ({
-                            ...streamer,
-                            description: e.target.value,
-                        }))
-                    }
-                />
-            </div>
-
-            <button type="submit">+ Add</button>
         </form>
     );
 }
